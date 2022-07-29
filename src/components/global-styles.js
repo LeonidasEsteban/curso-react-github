93b15649e504dcd4c7056b38a0c9801e129eddde
Leonidas Esteban <leonidasesteban@gmail.com>
2022-07-28 23:29:06 -0500

    clase 17

--- src/components/global-styles.js
+++ src/components/global-styles.js
@@ -14,14 +14,14 @@ const GlobalStylesStyled = createGlobalStyle`
     --pink: #cc68a0;
     --yellow: #f1e05a;
     --purple: #563d7c;
-    --headline1: semi bold 1.625rem/2rem Inter;
+    --headline1: 600 1.625rem/2rem Inter;
     --button: 500 0.875rem/1.5rem Inter;
-    --headline2-semi-bold: semi bold 1.25rem/1.5rem Inter;
+    --headline2-semi-bold: 600 1.25rem/1.5rem Inter;
     --headline2-ligth: 300 1.25rem/1.5rem Inter;
     --body1-regular: 400 1rem/1.5rem Inter;
     --body1-semi-bold: 700 1rem/1.5rem Inter;
     --body2-regular: 400 0.875rem/1.5rem Inter;
-    --body2-semi-bold: semi bold 0.875rem/1.5rem Inter;
+    --body2-semi-bold: 600 0.875rem/1.5rem Inter;
     --caption-regular: 400 0.75rem/1.125rem Inter;
     --caption-medium: 500 0.75rem/1.125rem Inter;
   }
--- src/components/profile.js
+++ src/components/profile.js
@@ -2,13 +2,74 @@ import styled from 'styled-components'
 
 const ProfileStyled = styled.div`
   grid-area: profile;
-  background-color: yellow;
+  /* background-color: yellow; */
+  .avatar {
+    border-radius: 50%;
+    border: 1px solid var(--grey-1);
+    overflow: hidden;
+    box-sizing: border-box;
+    margin-block-end: 1.5rem;
+  }
+  .name {
+    font: var(--headline1);
+    color: var(--white);
+    margin: 0;
+    margin-block-end: .5rem;
+  }
+  .username {
+    margin-block-start: .5rem;
+    margin-block-end: 1.5rem;
+    font: var(--headline2-ligth);
+  }
+  .info {
+    /* border: 1px solid red; */
+    color: var(--grey-1);
+    text-decoration: none;
+    display: flex;
+    align-items: center;
+    gap: .5rem;
+    margin-block: 1rem;
+    font: var(--body2-semi-bold);
+  }
+  a:hover {
+    text-decoration: underline;
+  }
+
+  .buttons {
+    display: flex;
+    gap: .5rem;
+    margin-block-end: 1.5rem;
+  }
 `
 
 function Profile() {
   return (
     <ProfileStyled>
-      Profile
+      <img src="" className='avatar' width="278" height="278" alt="" />
+      <p className="name">Leonidas Esteban</p>
+      <p className="username">LeonidasEsteban</p>
+      <div className="buttons">
+        <button>follow</button>
+        <button>sponsors</button>
+      </div>
+      <p className="bio info">
+        hola mundo!
+      </p>
+      <p className="follwers info">
+        • 10 <span>followers</span> <span>•</span> 15 <span>following</span>
+      </p>
+      <p className="stars info">
+        • 81
+      </p>
+      <p className="location info">
+        • CDMX
+      </p>
+      <a className="info" href="https://leonidasesteban.com" target="_blank" rel="noreferrer">
+        https://leonidasesteban.com
+      </a>
+      <a className="info" href="https://leonidasesteban.com" target="_blank" rel="noreferrer">
+        @LeonidasEsteban
+      </a>
     </ProfileStyled>
   )
 }
