7def91982095d2c003f78b3584ad0d5eee032998
Leonidas Esteban <leonidasesteban@gmail.com>
2022-09-16 16:00:30 -0500

    clase 25

--- src/components/repo-item.js
+++ src/components/repo-item.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import Language from './language'
+import Icon from './icon'
 
 const RepoItemStyled = styled.div`
   padding-block: 1rem;
@@ -43,9 +44,30 @@ const RepoItemStyled = styled.div`
     padding-block: .25rem;
     border-radius: 2rem;
   }
+
+  .details {
+    display: flex;
+    gap: 1rem;
+    font: var(--caption-regular);
+  }
+
+  .details-item {
+    display: flex;
+    gap: .5rem;
+    align-items: center;
+    color: var(--grey-1);
+    & span::first-letter {
+      text-transform: uppercase;
+    }
+}
 `
 
 function RepoItem(props) {
+  const updatedAt = new Date(props.updated_at)
+  const today = new Date()
+  const diffMilliseconds = updatedAt - today
+  const diffDays = Math.ceil(diffMilliseconds / (1000 * 60 * 60 * 24))
+  const timeAgo = new Intl.RelativeTimeFormat('es').format(diffDays, 'days')
   return (
     <RepoItemStyled>
       <h3 className="title">
@@ -74,9 +96,22 @@ function RepoItem(props) {
           </div>
         ) : null
       }
-      {
-        props.language ? <Language name={props.language} /> : null
-      }
+      <div className="details">
+        {
+          props.language ? <Language name={props.language} /> : null
+        }
+        <span className="details-item">
+          <Icon name="star" />
+          <span>{props.stargazers_count}</span>
+        </span>
+        <span className="details-item">
+          <Icon name="branch" />
+          <span>{props.forks_count}</span>
+        </span>
+        <span className="details-item">
+          <span>{timeAgo}</span>
+        </span>
+      </div>
 
     </RepoItemStyled>
   )
