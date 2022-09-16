bf513246ed693e6bee5898b91a7bb42b9e0a3814
Leonidas Esteban <leonidasesteban@gmail.com>
2022-09-16 17:45:37 -0500

    clase 28

--- src/App.js
+++ src/App.js
@@ -3,25 +3,20 @@ import Profile from './components/profile';
 import Filters from './components/filters';
 import RepoList from './components/repo-list';
 import Search from './components/search';
-import repoData from './components/repo-data';
 import { useState, useEffect } from 'react'
 import { getUser, getRepos } from './services/users'
-// const repoList = [
-//   {
-//     name: 'mi primer proyecto con react',
-//     id: 123,
-//   },
-//   {
-//     name: 'mi segundos proyecto con react',
-//     id: 124,
-//   }
-// ]
+import { useParams } from 'react-router-dom'
 
 function App() {
+  const params = useParams()
+  let username = params.user
+  if (!username) {
+    username = 'leonidasesteban'
+  }
   const [user, setUser] = useState({})
   const [repos, setRepos] = useState([])
   useEffect(() => {
-    getUser('leonidasesteban').then(({ data, isError }) => {
+    getUser(username).then(({ data, isError }) => {
       if (isError) {
         console.log('no hemos encontrado a este crack')
         return
@@ -31,7 +26,7 @@ function App() {
 
   }, [])
   useEffect(() => {
-    getRepos('leonidasesteban').then(({ data, isError }) => {
+    getRepos(username).then(({ data, isError }) => {
       if (isError) {
         console.log('no hemos encontrado los repos de este crack')
         return
